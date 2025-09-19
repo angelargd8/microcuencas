151187bddfd0f2e475ee628c2f760c15a162ba25
Paula Barillas <106341373+paulabaal12@users.noreply.github.com>
2025-09-19 10:49:07 -0600

    fix: updated about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -11,36 +11,40 @@ export default function About() {
       <Navbar />
       
       <main className={`${styles.main} ${aboutStyles.aboutContainer}`}>
-        <div className={aboutStyles.titleSection}>
-          <h1 className={aboutStyles.header}>Acerca de Nosotros</h1>
-          <p className={aboutStyles.description}>
-            En "Salva a la Microcuenca", somos un equipo apasionado de ecologistas, educadores y voluntarios 
-            comprometidos con la conservación de nuestros recursos hídricos. Nuestra organización nace de la
-            necesidad urgente de proteger las microcuencas, ecosistemas fundamentales que sustentan la vida en nuestra Tierra.
-            Juntos, podemos marcar la diferencia y salvar nuestras microcuencas.
-          </p>
-        </div>
-
-        <div className={aboutStyles.section}>
-          <h2 className={aboutStyles.sectionTitle}>Misión</h2>
-          <p className={aboutStyles.sectionContent}>
-            Nuestra misión es proteger y restaurar las microcuencas a través de la 
-            educación, la participación comunitaria y la implementación de 
-            prácticas sostenibles. Trabajamos para garantizar el acceso a agua 
-            limpia y saludable, promoviendo la biodiversidad y el equilibrio 
-            ecológico en nuestras comunidades.
-          </p>
-        </div>
-          
-        <div className={aboutStyles.section}>
-          <h2 className={aboutStyles.sectionTitle}>Visión</h2>
-          <p className={aboutStyles.sectionContent}>
-            Nuestra visión es proteger y restaurar las microcuencas a través de la 
-            educación, la participación comunitaria y la implementación de 
-            prácticas sostenibles. Trabajamos para garantizar el acceso a agua 
-            limpia y saludable, promoviendo la biodiversidad y el equilibrio 
-            ecológico en nuestras comunidades.
-          </p>
+        <div className={aboutStyles.aboutGrid}>
+          <div className={aboutStyles.aboutLeft}>
+            <div className={aboutStyles.titleSection}>
+              <h1 className={aboutStyles.header}>Acerca de Nosotros</h1>
+              <p className={aboutStyles.description}>
+                En "Salva a la Microcuenca", somos un equipo apasionado de ecologistas, educadores y voluntarios 
+                comprometidos con la conservación de nuestros recursos hídricos. Nuestra organización nace de la
+                necesidad urgente de proteger las microcuencas, ecosistemas fundamentales que sustentan la vida en nuestra Tierra.
+                Juntos, podemos marcar la diferencia y salvar nuestras microcuencas.
+              </p>
+            </div>
+          </div>
+          <div className={aboutStyles.aboutRight}>
+            <div className={aboutStyles.section}>
+              <h2 className={aboutStyles.sectionTitle}>Misión</h2>
+              <p className={aboutStyles.sectionContent}>
+                Nuestra misión es proteger y restaurar las microcuencas a través de la 
+                educación, la participación comunitaria y la implementación de 
+                prácticas sostenibles. Trabajamos para garantizar el acceso a agua 
+                limpia y saludable, promoviendo la biodiversidad y el equilibrio 
+                ecológico en nuestras comunidades.
+              </p>
+            </div>
+            <div className={aboutStyles.section}>
+              <h2 className={aboutStyles.sectionTitle}>Visión</h2>
+              <p className={aboutStyles.sectionContent}>
+                Nuestra visión es proteger y restaurar las microcuencas a través de la 
+                educación, la participación comunitaria y la implementación de 
+                prácticas sostenibles. Trabajamos para garantizar el acceso a agua 
+                limpia y saludable, promoviendo la biodiversidad y el equilibrio 
+                ecológico en nuestras comunidades.
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
